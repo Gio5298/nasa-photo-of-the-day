d3a5272e3566f3e7cca11fd4e865fd60ae6b0be9
Regino P <regino5298@gmail.com>
2019-11-07 16:47:09 -0700

    added styling to Photocard

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import React, {useState} from "react";
+import React from "react";
 import "./App.css";
 import Cardholder from "./Components/Cardholder";
 
--- src/Components/Photocard.js
+++ src/Components/Photocard.js
@@ -1,18 +1,76 @@
-import React from "react"
+import React from "react";
+import styled from "styled-components";
 
+const BodyDiv = styled.div`
+  background-color: #7a7a7a;
+`;
+
+const NavBar = styled.div`
+  background-color: rgb(91, 93, 172);
+`;
+
+const SiteTitle = styled.h1`
+  font-size: 2rem;
+  text-align: center;
+  padding: .6rem;
+  color: white;
+`;
+
+const TopContent = styled.div`
+  margin: 1.8rem .8rem 0 0;
+`;
+
+const ApodTitle = styled.h2`
+  font-size:1.7rem;
+  text align: center;
+  margin: .5rem 0;
+  color: white;
+`;
+
+const ApodDate = styled.h3`
+  font-size:1.4rem;
+  text align: center;
+  margin: .5rem 0;
+  color: white;
+`;
+
+const CenterContent = styled.div`
+  display: flex;
+`;
+
+const SpaceImg = styled.img`
+  width: 40%;
+  height: 40%;
+  margin: 1.5rem;
+  border-radius: 5px;
+`;
+
+const DescriptContent = styled.div`
+  display: flex;
+  flex-direction: column;
+  text-align: center;
+  padding: 3rem;
+  margin: 5rem 0;
+`;
 
 const PhotoCard = props => {
     return(
-        <div>
-            <h1>NASA Astronomy Picture Of The Day</h1>
-            <h2>{props.title}</h2>
-            <h3>{props.date}</h3>
-            <img src= {props.imgurl} alt="Nasa Astronomy Image"/>
-            <div>
+        <BodyDiv>
+            <NavBar>
+                <SiteTitle>NASA Astronomy Picture Of The Day</SiteTitle>
+            </NavBar>
+            <TopContent>
+            <ApodTitle>{props.title}</ApodTitle>
+            <ApodDate>{props.date}</ApodDate>
+            </TopContent>
+            <CenterContent>
+            <SpaceImg src= {props.imgurl} alt="Nasa Astronomy Image"/>
+            <DescriptContent>
                 <h3>Description</h3>
                 <p>{props.explanation}</p>
-            </div>
-        </div>
+            </DescriptContent>
+            </CenterContent>
+        </BodyDiv>
     );
 };
 
